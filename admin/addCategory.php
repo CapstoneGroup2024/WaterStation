4d2bf2613b4fc16d8ae27866bc4b9af5b5e6ba0c
denisesarona <135094209+denisesarona@users.noreply.github.com>
2024-04-22 01:55:51 +0800

    added comments

--- admin/addCategory.php
+++ admin/addCategory.php
@@ -79,5 +79,5 @@
         </div>
     </div>
 </div>
-
+<!--------------- FOOTER --------------->
 <?php include('includes/footer.php');?>
--- admin/codes.php
+++ admin/codes.php
@@ -3,7 +3,7 @@
 include('../config/dbconnect.php');
 include('../functions/myAlerts.php');
 
-if(isset($_POST['addCateg_button'])){ 
+if(isset($_POST['addCateg_button'])){ // IF FORM SUBMIT IS FROM addCateg_button
     $name = $_POST['name'];
     $slug = $_POST['slug'];
     $description = $_POST['description'];
@@ -11,29 +11,27 @@ if(isset($_POST['addCateg_button'])){
     $meta_title = $_POST['meta_title'];
     $meta_description = $_POST['meta_description'];
     $meta_keywords = $_POST['meta_keywords'];
-    $status = isset($_POST['status']) ? '1':'0'; // If the status is not null
-    $popular = isset($_POST['popular']) ? '1':'0'; // If popular is not null
+    $status = isset($_POST['status']) ? '1':'0'; // IF THE STATUS IS SET AND NOT NULL
+    $popular = isset($_POST['popular']) ? '1':'0'; // IF THE POPULAR IS SET AND NOT NULL
     
-    $image = $_FILES['image']['name']; 
+    $image = $_FILES['image']['name']; // GET THE ORIGINAL NAME OF THE UPLOADED FILE 
 
-    $path = "../uploads"; // IMAGES FOLDER
-    $image_ext = pathinfo($image, PATHINFO_EXTENSION); // GIVE THE IMAGE EXTENSION
-    $filename = time().'.'.$image_ext;
-
+    $path = "../uploads"; // DEFINE THE DIRECTORY WHERE UPLOADED IMAGES IN WILL BE STORED 
+    
+    $image_ext = pathinfo($image, PATHINFO_EXTENSION); // GET THE FILE EXTENSION OF THE UPLOADED IMAGE 
+    $filename = time().'.'.$image_ext; // GENERATE A UNIQUE FILENAME FOR THE UPLOADED IMAGE BY APPEDING THE CURRENT TIMESTAMP AND THE ORIGINAL FILE EXT
+    
     $categ_query = "INSERT INTO categories
-    (name,slug,description,meta_title,meta_description,meta_keywords,status,popular,image)
-    VALUES ('$name','$slug','$description','$meta_title','$meta_description','$meta_keywords','$status','$popular','$filename')";
-
-    $categ_query_run = mysqli_query($con, $categ_query);
-
+        (name,slug,description,meta_title,meta_description,meta_keywords,status,popular,image)
+        VALUES ('$name','$slug','$description','$meta_title','$meta_description','$meta_keywords','$status','$popular','$filename')"; 
+    
+    $categ_query_run = mysqli_query($con, $categ_query); // EXECURE THE SQL QUERY TO INSERT CATEGORY INFORMATION INTO THE DATABASE 
+    
     if($categ_query_run){
-        move_uploaded_file($_FILES['image']['tmp_name'], $path.'/'.$filename);
-        redirect("addCategory.php", "Category added successfully");
+        move_uploaded_file($_FILES['image']['tmp_name'], $path.'/'.$filename); // MOVE THE UPLOADED IMAGE FILE FROM THE TEMPORARY DIRECTORY TO THE SPECIFIED UPLOAD DIRECTORY WITH GENERATED FILE NAME 
+        redirect("addCategory.php", "Category added successfully"); 
     } else{
-        redirect("addCategory.php", "Something went wrong");
+        redirect("addCategory.php", "Something went wrong"); 
     }
-
-
 }
-
 ?>
